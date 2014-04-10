c3f649648eea24d7e435c85e4bc005a65044a7d3
Gregory Sheppard <gregory.sheppard@live.com>
2014-04-09 21:41:24 -0400

    Added forms to views

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -10,25 +10,47 @@ class SchedulesController < ApplicationController
     @schedule = Schedule.new
 
     @sunday = int_to_time(params[:sunday])
+    @upcoming_weeks = []
+    4.times { |x| @upcoming_weeks << @sunday + (x+1).weeks }
     @weekdays = [@sunday]
     6.times { |x| @weekdays << @sunday + (x+1).days }
 
     @schedules = Schedule.where(user: current_user).where(start_time: @sunday..(@sunday + 6.days))
     @employees = Employee.where(user: current_user)
 
-    @emp_schedules = {}
+    @shifts = {}
+    7.times do |day_index|
+      @schedules.each do |sched|
+        @shifts[sched.employee] ||= []
 
-    @schedules.each do |sched|
-      @employees.each do |emp|
-        if sched.employee == emp
-          if !@emp_schedules[emp].is_a?(Array)
-            @emp_schedules[emp] = [sched]
-          else
-            @emp_schedules[emp] << sched
-          end
+        if sched.start_time.wday == day_index
+          @shifts[sched.employee][day_index] ||= sched
+        else
+          @shifts[sched.employee][day_index] ||= Schedule.new(employee: sched.employee)
         end
       end
     end
+
+  end
+
+  def week_create
+    sunday = params[:create_schedules][:weeks].match(/(\d{4}\-\d{2}\-\d{2})/).to_s.split('-')
+    sunday.map! { |x| x.to_i }
+
+    @employees = Employee.where(user: current_user)
+
+    @employees.each do |emp|
+      7.times do |num|
+
+        Schedule.create(
+          user: current_user,
+          employee: emp,
+          start_time: DateTime.new(sunday[0], sunday[1], sunday[2]+num),
+          end_time: DateTime.new(sunday[0], sunday[1], sunday[2]+num)
+          )
+      end
+    end
+    binding.pry
 
   end
 
@@ -42,6 +64,7 @@ class SchedulesController < ApplicationController
     end
   end
 
+  #############################
   private
   def int_to_time(int)
     int = int.to_s.split('')
--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,5 @@ Dialgus::Application.routes.draw do
   resources :schedules, only: [:index, :show, :create]
 
   get 'schedules/week/:sunday', to: 'schedules#week', as: :schedule_week
+  post 'schedules/week/:sunday', to: 'schedules#week_create', as: :new_schedule_week
 end
